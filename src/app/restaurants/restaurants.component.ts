5605f1b2c50c0b3f8c553dd9c411729fbec03980
Daniel Santos <dmsdaniel@gmail.com>
2019-01-27 20:06:13 -0200

    NÃO QUEBRAR QDO ERRO BACKEND

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -8,23 +8,29 @@ import 'rxjs/add/operator/switchMap'
 import 'rxjs/add/operator/do'
 import 'rxjs/add/operator/debounceTime'
 import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
+import { Observable } from 'rxjs/Observable';
+
+
 
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
   animations: [
-    trigger('toogleSearch',[
+    trigger('toogleSearch', [
       state('hidden', style({
         opacity: 0,
-        "max-heigth": "0px" })),
+        "max-heigth": "0px"
+      })),
       state('visible', style({
-          opacity: 1,
-          "max-height": "70px",
-          "margin-top": "20px"
-         })),
-         transition('* => *', animate('0ms ease-in-out'))
-        ])
-    ]
+        opacity: 1,
+        "max-height": "70px",
+        "margin-top": "20px"
+      })),
+      transition('* => *', animate('0ms ease-in-out'))
+    ])
+  ]
 
 })
 export class RestaurantsComponent implements OnInit {
@@ -35,25 +41,27 @@ export class RestaurantsComponent implements OnInit {
   searchControl: FormControl
 
   constructor(private restaurantsService: RestaurantsService,
-              private fb: FormBuilder) { }
-  
+    private fb: FormBuilder) { }
+
   ngOnInit() {
     this.searchControl = this.fb.control('')
     this.searchForm = this.fb.group({
       searchControl: this.searchControl
     })
     this.searchControl.valueChanges
-    .debounceTime(500)
-    .distinctUntilChanged()
-    .do(searchTerm => console.log(`q=${searchTerm}`))
-    .switchMap(searchTerm => 
-       this.restaurantsService.restaurants(searchTerm))
-    .subscribe(restaurants => this.restaurants = restaurants)
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .do(searchTerm => console.log(`q=${searchTerm}`))
+      .switchMap(searchTerm =>
+        this.restaurantsService
+          .restaurants(searchTerm)
+          .catch(error=>Observable.from([])))
+      .subscribe(restaurants => this.restaurants = restaurants)
 
     this.restaurantsService.restaurants()
       .subscribe(restaurants => this.restaurants = restaurants)
   }
-  toggleSearch(){
+  toggleSearch() {
     this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden'
   }
 } 
