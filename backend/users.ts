ef6cea287ae479d7d7617447804c269751039666
Daniel Santos <dmsdaniel@gmail.com>
2019-01-28 23:09:57 -0200

    jwt login

---
+++ backend/users.ts
@@ -0,0 +1,14 @@
+export class User {
+    constructor(public email: string, 
+                public name: string, 
+                private password: string){} 
+    matches(another: User): boolean {
+        return another !== undefined &&
+               another.email === this.email && 
+               another.password === this.password
+    }
+}
+export const users = {
+    "daniel@gmail.com": new  User('daniel@gmail.com','daniel','senha'),
+    "pedro@gmail.com": new  User('pedro@gmail.com','pedro','senha')
+}
